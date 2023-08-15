c95c87dbbed8d9403e8d267f1ef77e479db5e64a
윤국현 <djaudwls7873@gmail.com>
2023-08-16 08:56:29 +0900

    🌈feat : layout 생성하기

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
+
+import styles from "./Layout.module.scss";
+
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className={styles.layout}>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
