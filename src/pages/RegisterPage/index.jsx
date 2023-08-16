1375c031814256c70895613078aad182e36e8d3a
윤국현 <djaudwls7873@gmail.com>
2023-08-16 09:19:59 +0900

    🌈feat : login, register 페이지 ui 생성하기

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="page">
+      <div className="form_container">
+        <h1>회원가입</h1>
+        <p>
+          이미 계정이 있습니까? <Link to={"/login"}>로그인</Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
