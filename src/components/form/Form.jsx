206f9631e6506c3603f5457285d78204074f7fd4
윤국현 <djaudwls7873@gmail.com>
2023-08-16 13:54:41 +0900

    🌈feat : form 컴포넌트 생성 (react-hook-form)

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,63 @@
-const Form = () => {
-  return <div>Form</div>;
+import styles from "./Form.module.scss";
+import { useForm } from "react-hook-form";
+
+const Form = ({ title }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmit = ({ email, password }) => {
+    console.log(email, password);
+  };
+
+  const userEmail = {
+    required: "필수 입력 사항입니다.",
+    pattern: {},
+  };
+
+  const userPassword = {
+    required: "필수 입력 사항입니다.",
+    minLength: {
+      value: 4,
+      message: "4자리 이상 입력해주세요.",
+    },
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+      <div>
+        <input
+          type="email"
+          placeholder="E-mail"
+          {...register("email", userEmail)}
+        />
+        {errors?.email && (
+          <div>
+            <span className={styles.form_error}>{errors.email.message}</span>
+          </div>
+        )}
+      </div>
+      <div>
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password", userPassword)}
+        />
+        {errors?.password && (
+          <div>
+            <span className={styles.form_error}>{errors.password.message}</span>
+          </div>
+        )}
+      </div>
+      <button type="submit">{title}</button>
+      <span className={styles.form_error}></span>
+    </form>
+  );
 };
 
 export default Form;
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import SignIn from "./sign-in/SignIn";
 
 const LoginPage = () => {
   return (
     <div className="page">
       <div className="form_container">
         <h1>로그인</h1>
+        <SignIn />
         <p>
           계정이 없습니까? <Link to={"/register"}>가입하기</Link>
         </p>
