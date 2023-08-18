374e8b87a2e7b5af370b3fac5ab21c8596acf568
윤국현 <djaudwls7873@gmail.com>
2023-08-18 21:27:40 +0900

    🌈feat : 상세 페이지 데이터 생성

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAppDispatch } from "../../hooks/redux";
+import { fetchProduct } from "../../store/products/product.slice";
+
 const DetailPage = () => {
+  const { id } = useParams();
+  const productId = Number(id);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProduct(productId));
+  }, [productId]);
   return <div>DetailPage</div>;
 };
 
