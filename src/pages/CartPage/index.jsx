7f0707abbe95c7d60f1eeebad189dfabf32594ee
윤국현 <djaudwls7873@gmail.com>
2023-08-19 16:44:22 +0900

    🌈feat : 장바구니 페이지 생성

--- src/pages/CartPage/index.jsx
+++ src/pages/CartPage/index.jsx
@@ -1,5 +1,21 @@
+import CartEmpty from "../../components/cart-empty/CartEmpty";
+import { useAppSelector } from "../../hooks/redux";
+
 const CartPage = () => {
-  return <div>CartPage</div>;
+  const { products } = useAppSelector((state) => state.cartSlice);
+  return (
+    <div className="page">
+      {!products.length ? (
+        <CartEmpty title="Cart" />
+      ) : (
+        <div className="container">
+          <h1>장바구니</h1>
+          <CartList />
+          <Checkout />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CartPage;
